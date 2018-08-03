c6bdf4cd847e017fb21de48f0bbc2aa8ba7835e6
Raffi Kudlac <raffikudlac@MacBook-Pro.hitronhub.home>
2018-08-02 19:00:50 -0700

    find distance to closest zero

--- src/medium/Run.java
+++ src/medium/Run.java
@@ -1,13 +1,13 @@
 package medium;
 
 import helper.SolutionOutline;
-import medium.questions.*;
+import medium.questions.Matrix01;
 
 public class Run {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SolutionOutline solution = new ReachANumber();				
+		SolutionOutline solution = new Matrix01();
 		solution.runTest();				
 	}
 }
