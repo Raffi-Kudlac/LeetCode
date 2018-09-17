8f7c63a9b67d09bcf31473d3adeb4d98c21e0a7f
Raffi Kudlac <raffi.kudlac@gmail.com>
2018-09-16 18:49:36 -0700

    find next node in a BST

--- src/CrackIngTheCodingInterview/Run.java
+++ src/CrackIngTheCodingInterview/Run.java
@@ -1,12 +1,13 @@
-package CrackIngTheCodingInterview.Chapter3;
+package CrackIngTheCodingInterview;
 
+import CrackIngTheCodingInterview.Chapter4.FindNextNode;
 import helper.SolutionOutline;
 
 public class Run {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        SolutionOutline solution = new SortStack();
+        SolutionOutline solution = new FindNextNode();
         solution.runTest();
     }
 }
